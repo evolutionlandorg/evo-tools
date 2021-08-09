e4117cd7fbb30bababa749fb236d4a42edb78ff9
echo <hujw77@gmail.com>
2021-08-09 20:00:08 +0800

    suport polygon network

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {}
 , dappPkgs ? import (pkgs.fetchgit {
     url = "https://github.com/hujw77/dapptools";
-    rev = "evo/0.0.1";
-    sha256 = "131pzawyyhlapjhrzjl4d3byigvc38y4b51yvzams8k85b98iw18";
+    rev = "evo/0.0.2";
+    sha256 = "0vwjhfkc4k5mpwj6p9b6c67sr2mzwajayijdwcqwcjgkg4kc67si";
     fetchSubmodules = true;
   }) {}
 }:
