eacb5a6b73002618bf7f9c082baa54b3537cc867
echo <hujw77@gmail.com>
2022-01-12 16:35:46 +0800

    fix mismatch hash

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 , dappPkgs ? import (pkgs.fetchgit {
     url = "https://github.com/hujw77/dapptools";
     branchName = "master";
-    rev= "7a6e534e4dd9c37e0ec36840cbf7611548b5fbe6";
+    sha256 = "At8uzX2+e7lcnidmDni8icv/KG/1lPHIhGEv3AFabPM="
     fetchSubmodules = true;
   }) {}
 }:
