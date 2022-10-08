a804e4e8ff7474776044e7af538967e33529693e
echo <hujw77@gmail.com>
2022-10-08 11:31:40 +0800

    evo/0.0.3

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {}
 , dappPkgs ? import (pkgs.fetchgit {
     url = "https://github.com/hujw77/dapptools";
-    branchName = "master";
-    sha256 = "1vi7v2a8my7lmh9y84pld2lkml0hi2l4d6n47fw4kycpznlfc7ly";
+    branchName = "evo/0.0.3";
+    sha256 = "140apalhrp4njcyh2r79393jfzisivi8vjxirgcp0x792kavbkyr";
     fetchSubmodules = true;
   }) {}
 }:
